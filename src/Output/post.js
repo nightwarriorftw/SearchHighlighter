13f49d0a36d60e9dbe6f637f61494f631d7e8f15
nightwarrior-xxx <amanv1999@gmail.com>
2020-03-09 13:32:58 +0530

    Added changes to index.js

--- src/Output/post.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-export function Post({ content, index }) {
-  return (
-      <div>{content}</div>
-  );
-}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { SearchPage } from "./SearchBar/searchbar";
+/*eslint-env jquery*/
+class SearchArea extends React.Component {
+  // Constructor
+  constructor() {
+      super()
+      this.state = {txt: '', results: 0}
+      this.update = this.update.bind(this)
+    }
+    // Update is bound to input's onClick event
+  update(e) {
+      // Helper vars
+      var search = e.target.value
+      var regex = new RegExp(search, "gi")
+      var str = $('pre').text()
+        // Print occurances
+      output(
+          str,
+          regex,
+          document.querySelector('pre')
+        )
+        // Find num matches
+      var matches = document.querySelectorAll('#hl').length
+        // Set state accordingly
+      this.setState({
+        txt: e.target.value,
+        results: (matches === str.length + 1) ? 0 : matches // Hack 
+      });
+    }
+    // Render 
+  render() {
+    return ( 
+      <div>
+      <SearchBox txt={this.state.txt} update={this.update}/> 
+      <SearchResults result={this.state.results} /> 
+      </div>
+    );
+  }
+}
 
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <SearchPage />
+/**
+ * Search Box component
+ */
+const SearchBox = (props) => {
+  return ( 
+    <div id="searchbox">
+    <input type="text"
+    onChange={props.update}/> 
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+/**
+ * Search Box component
+ */
+const SearchResults = (props) => {
+  return ( 
+    <div id="results">
+      <p>{props.result} results found. </p> 
+    </div>
+  );
+}
+
+/**
+ * (ES6) Prints the text and
+ * highlights the search query
+ * if found. Uses regex.
+ */
+const output = (str, regex, target) => {
+  target.innerHTML =
+  str.replace(
+    regex,
+    str =>
+    `<span id="hl">${str}</span>`
+  );
+}
+
+ReactDOM.render(<SearchArea />,
+    document.getElementById('searcharea')
+);
